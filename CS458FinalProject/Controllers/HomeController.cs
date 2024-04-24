9937eea931ebd6b6008924a4f247198fbedda121
Peyton Hopkins <peytonlukehopkins@outlook.com>
2024-04-23 19:37:56 -0500

    Maxs additions

---
+++ CS458FinalProject/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CS458FinalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Register";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Login";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize()
+        {
+            // get values from form 
+            string username = Request.Form["username"];
+            string password = Request.Form["password"];
+            string userType = Request.Form["userType"];
+
+            if (userType == "User")
+            {
+            }
+
+            else if (userType == "Contractor")
+            {
+            }
+
+            return View("Login");
+        }
+    }
+}
---
+++ CS458FinalProject/Data/CS458FinalProjectContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CS458FinalProject.Data
+{
+    public class CS458FinalProjectContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CS458FinalProjectContext() : base("name=CS458FinalProjectContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<CS458FinalProject.Contractor> Contractors { get; set; }
+
+        public System.Data.Entity.DbSet<CS458FinalProject.User> Users { get; set; }
+
+        public System.Data.Entity.DbSet<CS458FinalProject.Project> Projects { get; set; }
+    }
+}
